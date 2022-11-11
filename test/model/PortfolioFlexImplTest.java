8cb3e977490212cb56bdbbae2cb0dc3b458d203c
Nitin <ntnbhat9@gmail.com>
2022-11-11 17:35:25 -0500

    tests

--- test/model/PortfolioFlexImplTest.java
+++ test/model/PortfolioFlexImplTest.java
@@ -1,10 +1,13 @@
 package model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.sound.sampled.Port;
 
 import utils.Utils;
 
@@ -12,63 +15,50 @@ import static org.junit.Assert.*;
 
 public class PortfolioFlexImplTest {
 
-  @Test
-  public void testPortfolioFlexImplConstructor1() throws Exception {
+  Map<String, Map<String, Double>> order;
+
+  @Before
+  public void beforeSuit() throws Exception {
     Utils.clearStockDirectory();
-    Map<String, Map<String, Double>> order = new HashMap<>();
+    order = new HashMap<>();
     Map<String , Double> ciscoTuple = new HashMap<>();
-    ciscoTuple.put("2021-11-28" , 10.0);
-    ciscoTuple.put("2021-11-29" , 12.0);
-    ciscoTuple.put("2021-11-31" , -10.0);
+    ciscoTuple.put("2022-11-28" , 10.0);
+    ciscoTuple.put("2022-11-29" , 12.0);
+    ciscoTuple.put("2022-11-31" , -10.0);
+    Map<String , Double> netflixTuple = new HashMap<>();
+    netflixTuple.put("2022-11-28" , 10.0);
+    netflixTuple.put("2022-11-29" , -2.0);
+    netflixTuple.put("2022-11-31" , 2.0);
+    Map<String , Double> appleTuple = new HashMap<>();
+    appleTuple.put("2020-11-28" , 10.0);
+    appleTuple.put("2021-11-29" , -2.0);
+    appleTuple.put("2022-11-31" , 2.0);
     order.put("CSCO" , ciscoTuple);
+    order.put("NFLX" , netflixTuple);
+    order.put("AAPL" , appleTuple);
+  }
 
-    // test constructor creator
-    PortfolioFlex p = new PortfolioFlexImpl(order , "flexTest");
 
-    //test get summary
-    Map<String , Double> s = p.getPortfolioSummary("2021-11-31");
-    for(String key : s.keySet()){
-      System.out.println(key+" : "+s.get(key));
-    }
+ @Test
+ public void testPortfolioFlexImplCreatorConst() throws Exception{
+   PortfolioFlex p = new PortfolioFlexImpl(order , "flexTest");
+ }
 
-    System.out.println("*********");
-    //test get val on date
-    System.out.println(p.getValueOnDate("2021-11-31"));
+  @Test(expected = Exception.class)
+  public void testPortfolioFlexImplCreatorConstFailure1() throws Exception {
+    PortfolioFlex p = new PortfolioFlexImpl(null , "flexTest");
+  }
 
-    // test cost basis
-    System.out.println(p.getCostBasis("2021-11-31"));
+  @Test(expected = Exception.class)
+  public void testPortfolioFlexImplCreatorConstFailure2() throws Exception {
+    PortfolioFlex p = new PortfolioFlexImpl(order , null);
+  }
 
-    System.out.println("*********");
-
-    // test getState
-    Map<String , AbstractMap.SimpleEntry<String , Double>> stateMap = p.getLatestState();
-    for(String key : stateMap.keySet()){
-      System.out.println(key+" : "+stateMap.get(key).getKey() + " , " + stateMap.get(key).getValue());
-    }
-
-    System.out.println("*********");
-    //test buy
-    AbstractMap.SimpleEntry<String, Double> updatedEntry = new AbstractMap.SimpleEntry<>("2021-11-31",20.0);
-    AbstractMap.SimpleEntry<String, AbstractMap.SimpleEntry<String, Double>> finalEntry = new AbstractMap.SimpleEntry<>("NFLX",updatedEntry);
-    p.addStock(finalEntry);
-
-    s = p.getPortfolioSummary("2021-11-31");
-    for(String key : s.keySet()){
-      System.out.println(key+" : "+s.get(key));
-    }
-
-    System.out.println("*********");
-    //test sell
-
-    AbstractMap.SimpleEntry<String, Double> updatedEntry2 = new AbstractMap.SimpleEntry<>("2021-11-31",-2.0);
-    AbstractMap.SimpleEntry<String, AbstractMap.SimpleEntry<String, Double>> finalEntry2 = new AbstractMap.SimpleEntry<>("CSCO",updatedEntry2);
-    p.sellStock(finalEntry2);
-
-    s = p.getPortfolioSummary("2021-11-31");
-    for(String key : s.keySet()){
-      System.out.println(key+" : "+s.get(key));
-    }
+  @Test
+  public void testPortfolioFlexImplRetreiverConst() throws Exception{
 
   }
+
+
 
 }
